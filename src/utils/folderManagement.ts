226ab3d0d0fd90b26e3a13e9013704bf8e23a035
Ricardo Pinto <ricardoschillermail@gmail.com>
2024-03-21 10:32:51 +0000

    Updated code

--- src/utils/folderManagement.ts
+++ src/utils/folderManagement.ts
@@ -1,19 +1,54 @@
 import * as vscode from 'vscode';
 
-export async function createFolder(selectedFolder: vscode.QuickPickItem |undefined ,folderName:string):Promise<boolean | undefined>{
-    if(selectedFolder !== undefined){
-        var folderURI = vscode.Uri.parse(selectedFolder?.detail! + '/' + folderName);
-    }else{
-        return undefined;
-    }
-    
 
-    try{
-        await vscode.workspace.fs.stat(folderURI);
-        return false;
-    }catch(err){
-        vscode.workspace.fs.createDirectory(folderURI);
+class OpResult<T> { //define T as a class or primitive type
+
+    public OpResult(result:T|undefined, error:string|undefined) {
+        if(result !== undefined && error !== undefined) {
+            //throw exception
+        }
+
+        if(result) {
+            this._hasSucceeeded = true;
+            this._result = result;
+        }
+        else {
+            this._error = error;
+        }
+    }
+
+
+    private _hasSucceeeded:boolean = false;
+
+    private _result:T|undefined = undefined;
+
+    private _error:string|undefined = undefined;
+
+     hasSucceeded():boolean {
+        return this._hasSucceeeded;
+    }
+
+    getResult():T|undefined {
+        return this._result;
+    }
+
+    getError():string|undefined {
+        return this._error;
+    }
+}
+
+
+//TODO refactor this code to not use anything that comes from the "interface". This is a utils class, and shouldn't know about how folders are accessed
+export async function createFolder(selectedFolder: vscode.QuickPickItem, folderName:string):Promise<boolean>{
+    
+    let folderURI = vscode.Uri.parse(selectedFolder?.detail! + '/' + folderName);
+
+    try {
+        await vscode.workspace.fs.createDirectory(folderURI);
         return true;
+    }
+    catch {
+        return false;
     }
 }
 
